a73d0eb4195cdf9df07b30fa4a61b80b116e1c0f
psidae <akson.rill@gmail.com>
2020-04-12 14:49:03 +0300

    Cloudinary config

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Cloudinary.config do |config|
+  config.cloud_name = ENV['CLOUD_NAME']
+  config.api_key = ENV['CLOUD_API_KEY']
+  config.api_secret = ENV['CLOUD_API_SECRET']
+  config.secure = true
+  config.cdn_subdomain = true
+end
